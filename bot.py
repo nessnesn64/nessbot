68656572476bec3e51e9888f55f20b69437457e1
Ness <baileywalters21@gmail.com>
2020-09-28 15:18:05 -0400

    bot commands + test command

--- bot.py
+++ bot.py
@@ -7,19 +7,19 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN') #This grabs the discord token from a .env file within the same directory as the bot
 
-#client = discord.Client()
-bot = commands.Bot(command_prefix='!')
+client = discord.Client()
+#bot = commands.Bot(command_prefix='!')
 
-#@client.event
-#async def on_ready():
-#    print(f'{client.user} has connected to Discord!')
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
 
 
 
-@bot.command(name='ness_test')
-async def ness_test(test):
+#@bot.command(name='ness_test')
+#async def ness_test(test):
     
-    await test.send("test")
+#    await test.send("test")
 
 
 
@@ -28,5 +28,5 @@ async def ness_test(test):
 
 
 
-#client.run(TOKEN)
-bot.run(TOKEN)
+client.run(TOKEN)
+#bot.run(TOKEN)
